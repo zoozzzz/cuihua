dee4462ce7220906e8a5d0e3be0ba7363fcb7d58
Wing <cairz@mogulinker.com>
2022-08-01 17:43:45 +0800

    pref: 优化

--- src/pages/numBoom/index.tsx
+++ src/pages/numBoom/index.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Image } from '@tarojs/components'
-import { AtButton, AtInput, AtInputNumber, AtCurtain } from 'taro-ui';
+import { AtButton, AtModal, AtIcon, AtInputNumber, AtCurtain, AtToast, AtModalHeader, AtModalContent, AtModalAction} from 'taro-ui';
 import { useEffect, useState } from 'react'
 import drink from '../../assets/drink.jpeg';
 import { generateNum } from './utils';
@@ -10,6 +10,9 @@ const NumBoom = () => {
   const [currentValue, setCurrentValue] = useState<number | string>(0);
   const [bingo, setBingo] = useState(false);
   const [range, setRange] = useState([0, 100]);
+  const [error, setError] = useState('');
+  const [visibleRule, setVisibleRule] = useState(true);
+
   useEffect(() => {
 
   }, []);
@@ -30,6 +33,11 @@ const NumBoom = () => {
     }
 
     // 没中，继续
+    if (currentValue < range[0] || currentValue > range[1]) {
+      // 不在已猜中的范围
+      setError('识唔识玩噶7头！过晒界啦！');
+      return;
+    }
     if (currentValue < num) {
       setRange([+currentValue, range[1]]);
     } else {
@@ -42,15 +50,27 @@ const NumBoom = () => {
     setNum(0);
     setCurrentValue(0);
     setRange([0, 100]);
+    setError('');
   }
 
   return (
     <View className='wrapper'>
 
+    <AtModal
+      isOpened={visibleRule}
+      title='游戏规则'
+      cancelText='取消'
+      confirmText='确认'
+      // onClose={ () => setVisibleRule(false) }
+      onCancel={ () => setVisibleRule(false) }
+      onConfirm={ () => setVisibleRule(false) }
+      content='我叫你饮个时你就饮得噶啦'
+    />
+
       {/* 已经生成数字 && 没中 */}
       {!bingo && !!num && <View className='tips'>{`${range[0]} ~ ${range[1]}`}</View>}
       {
-        !!num && <View className='inputWrapper'><AtInputNumber onChange={onChange} type='number' value={currentValue} /></View>
+        !!num && <View className='inputWrapper'><AtInputNumber min={range[0]} max={range[1]} onChange={onChange} type='number' value={currentValue} /></View>
       }
       {
         num ? (
@@ -65,15 +85,24 @@ const NumBoom = () => {
         )
       }
 
+      <AtToast
+        isOpened={!!error}
+        text={error}
+        icon='blocked'
+      />
+
       {/* 中奖了 */}
       <AtCurtain
         isOpened={bingo}
         onClose={onClose}
       >
-        <Image
-          style='width:100%;height:250px'
-          src={drink}
-        />
+        <View style={{ position: 'relative' }}>
+          <Text style={{ position: 'absolute', top: '50%', left: "50%", transform: "translate(-50%, -50%)" }}>饮啦7头</Text>
+          <Image
+            style='width:100%;height:250px'
+            src={drink}
+          />
+        </View>
       </AtCurtain>
     </View>
   )
